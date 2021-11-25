5d8f91fde48627db23e3ea5584277c9137e3f83c
Tilio Schulze <tilio.schulze@gmail.com>
2021-11-25 18:31:16 +0100

    testing new branch

--- main.py
+++ main.py
@@ -1,53 +1,12 @@
-from typing import Any, Optional, List, Union
+from typing import Any, Dict, Optional, List, Union
 from fastapi import FastAPI
 from fastapi.params import Body, Query
 from pydantic import BaseModel
 from pydantic.fields import Field
-from pydantic.schema import schema
-from enum import Enum
+from constants import *
+from models import AttributeDescription, InstanceInfo, ContinuousFilter, CategoricalFilter, ContinuousConstraint, CategoricalConstraint
 
 app = FastAPI()
-
-# string and number definitions to re-use through the entire code
-row_limit = 20
-
-type = "type"
-values = "values"
-categorical = "categorical"
-continuous = "continuous"
-lower_bound = "lower_bound"
-upper_bound = "upper_bound"
-
-
-class AttributeNames(str, Enum):
-    '''This class is used to have a central definition of how the attributes are referenced'''
-
-    # attributes in the dataset
-    balance = "balance"
-    duration = "duration"
-    history = "history"
-    purpose = "purpose"
-    amount = "amount"
-    savings = "savings"
-    employment = "employment"
-    available_income = "available_income"
-    #status_sex = "status_sex"
-    residence = "residence"
-    assets = "assets"
-    age = "age"
-    other_loans = "other_loans"
-    housing = "housing"
-    previous_loans = "previous_loans"
-    job = "job"
-    #telephone = "telephone"
-    #foreign_worker = "foreign_worker"
-    other_debtors = "other_debtors"
-    people_liable = "people_liable"
-
-    # meta-attributes
-    id = "id"
-    NN_recommendation = "NN_recommendation"
-    NN_confidence = "NN_confidence"
 
 category_mapping = {
     "financial" : [AttributeNames.history, AttributeNames.savings, AttributeNames.balance, AttributeNames.available_income, AttributeNames.assets, AttributeNames.other_loans, AttributeNames.other_debtors, AttributeNames.previous_loans],
@@ -55,82 +14,98 @@ category_mapping = {
     "loan" : [AttributeNames.amount, AttributeNames.duration, AttributeNames.purpose, AttributeNames.people_liable]
 }
 
-attribute_constraints = {
-    # TODO: can all of this be done with Field() or schema() ??
-    AttributeNames.balance : {
+attribute_constraints = [
+    {
+        attr_name : AttributeNames.balance,
         type : categorical,
         values : [] #List[str]
     },
-    AttributeNames.duration : {
+    {
+        attr_name : AttributeNames.duration,
         type : continuous,
         lower_bound : 0, #float
         upper_bound : 1 #float
     },
-    AttributeNames.history : {
+    {
+        attr_name : AttributeNames.history,
         type : categorical,
         values : [] 
     },
-    AttributeNames.purpose : {
+    {
+        attr_name : AttributeNames.purpose,
         type : categorical,
         values : []
     },
-    AttributeNames.amount : {
+    {
+        attr_name : AttributeNames.amount,
         type : continuous,
         lower_bound : 0,
         upper_bound : 1
     },
-    AttributeNames.savings : {
+    {
+        attr_name : AttributeNames.savings,
         type : categorical,
         values : []
     },
-    AttributeNames.employment : {
+    {
+        attr_name : AttributeNames.employment,
         type : categorical,
         values : []
     },
-    AttributeNames.available_income : {
+    {
+        attr_name : AttributeNames.available_income,
         type : categorical,
         values : []
     },
-    AttributeNames.residence : {
+    {
+        attr_name : AttributeNames.residence,
         type : categorical,
         values : []
     },
-    AttributeNames.assets :{
+    {
+        attr_name : AttributeNames.assets,
         type : categorical,
         values : []
     },
-    AttributeNames.age : {
+    {
+        attr_name : AttributeNames.age,
         type : continuous,
         lower_bound : 16,
         upper_bound : 100
     },
-    AttributeNames.other_loans : {
+    {
+        attr_name : AttributeNames.other_loans,
         type : categorical,
         values : []
     },
-    AttributeNames.housing : {
+    {
+        attr_name : AttributeNames.housing,
         type : categorical,
         values : []
     },
-    AttributeNames.previous_loans : {
+    {
+        attr_name : AttributeNames.previous_loans,
         type : categorical,
         values : []
     },
-    AttributeNames.job : {
+    {
+        attr_name : AttributeNames.job,
         type : categorical,
         values : []
     },
-    AttributeNames.other_debtors : {
+    {
+        attr_name : AttributeNames.other_debtors,
         type : categorical,
         values : []
     },
-    AttributeNames.people_liable : {
+    {
+        attr_name : AttributeNames.people_liable,
         type : categorical,
         values : []
     }
     # TODO: fill in the rest of the constraints
     # TODO: use smart lower and upper bounds as they will be important for filtering
-}
+]
 
 # standard configuration for table view
 standard_attributes = [
@@ -141,59 +116,21 @@ standard_attributes = [
     AttributeNames.employment
 ]
 
-class ContinuousFilter(BaseModel):
-    '''Defines the format for a continuous filter request body'''
-    attribute_name: AttributeNames
-    lower_bound: float
-    upper_bound: float
-
-class CategoricalFilter(BaseModel):
-    '''Defines the format for a categorical filter request body'''
-    attribute_name: AttributeNames
-    values: List[str]
-
-class InstanceInfo(BaseModel):
-    '''Defines how a JSON response for an instance information should look like.
-    Maps the predefined names for attributes as the alias of the pydantic class keys.'''
-    
-    # TODO: change the types (most are strings now)
-    # TODO: add stuff like ai recommendation and confidence level
-    id : Optional[int] = Field(None ,alias=AttributeNames.id.value)
-    balance : Optional[str] = Field(None, alias=AttributeNames.balance.value)
-    duration : Optional[str] = Field(None, alias=AttributeNames.duration.value)
-    history : Optional[str] = Field(None, alias=AttributeNames.history.value)
-    purpose : Optional[str] = Field(None, alias=AttributeNames.purpose.value)
-    amount : Optional[str] = Field(None, alias=AttributeNames.amount.value)
-    savings : Optional[str] = Field(None, alias=AttributeNames.savings.value)
-    employment : Optional[str] = Field(None, alias=AttributeNames.employment.value)
-    available_income : Optional[str] = Field(None, alias=AttributeNames.available_income.value)
-    residence : Optional[str] = Field(None, alias=AttributeNames.residence.value)
-    assets : Optional[str] = Field(None, alias=AttributeNames.assets.value)
-    age : Optional[str] = Field(None, alias=AttributeNames.age.value)
-    other_loans : Optional[str] = Field(None, alias=AttributeNames.other_loans.value)
-    housing : Optional[str] = Field(None, alias=AttributeNames.housing.value)
-    previous_loans : Optional[str] = Field(None, alias=AttributeNames.previous_loans.value)
-    job : Optional[str] = Field(None, alias=AttributeNames.job.value)
-    other_debtors : Optional[str] = Field(None, alias=AttributeNames.other_debtors.value)
-    people_liable : Optional[str] = Field(None, alias=AttributeNames.people_liable.value)
-    NN_recommendation : Optional[bool] = Field(None, alias=AttributeNames.NN_recommendation.value)
-    NN_confidence : Optional[float] = Field(None, le=1, ge=0, alias=AttributeNames.NN_confidence.value)
-
-
-# return a list of instance infos for the table view in lvl 1
 @app.post("/table", response_model=List[InstanceInfo], response_model_exclude_none=True) # second parameter makes sure that unused stuff won't be included in the response
 def table_view(
     filter: Optional[List[Union[ContinuousFilter, CategoricalFilter]]] = None,
     attributes: List[AttributeNames] = standard_attributes,
-    sort_by: AttributeNames = Body(AttributeNames.id),
+    sort_by: AttributeNames = Body(AttributeNames.ident),
     limit: int = Body(row_limit),
     offset: int = Body(0)
 ):
+    '''Returns a list of "limit" instances for the table view from a specific offset. Can have filters and chosen attributes.'''
+    
     example_result = [
         {
             AttributeNames.amount : 3200,
             AttributeNames.duration : 24,
-            AttributeNames.id : 1,
+            AttributeNames.ident : 1,
             AttributeNames.age : 23,
             AttributeNames.employment : "between 1 and 4 years",
             AttributeNames.NN_recommendation : False,
@@ -202,7 +139,7 @@ def table_view(
         {
             AttributeNames.amount : 8000,
             AttributeNames.duration : 12,
-            AttributeNames.id : 2,
+            AttributeNames.ident : 2,
             AttributeNames.age : 47,
             AttributeNames.employment : "more than 7 years",
             AttributeNames.NN_recommendation : True,
@@ -214,15 +151,46 @@ def table_view(
 
 @app.get("/instance/{id}", response_model=InstanceInfo)
 def get_entire_instance_by_id(id: int):
+    '''Returns an entire instance information for the lvl2 view'''
     example_output = {
         AttributeNames.amount : 8000,
         AttributeNames.duration : 12,
         AttributeNames.assets : "example_value",
         AttributeNames.available_income : "example_value" ,
-        AttributeNames.id : 2,
+        AttributeNames.ident : id,
         AttributeNames.age : 47,
         AttributeNames.employment : "more than 7 years",
         AttributeNames.NN_recommendation : True,
         AttributeNames.NN_confidence : 0.93
+    }
+    return example_output 
+
+@app.get("/attribute/constraints", response_model=List[Union[CategoricalConstraint, ContinuousConstraint]])
+def get_attribute_constraints():
+    '''Returns a JSON with the constraints for each attribute.'''
+    return attribute_constraints
+
+@app.get("attributes/descriptions", response_model=List[AttributeDescription])
+def get_attribute_descriptions():
+    example_output = {
+        AttributeNames.balance : "The current balance of the applicant's checking account (in Euro)",
+        AttributeNames.duration : "The duration of the loan (in months)",
+        AttributeNames.history : "...",
+        AttributeNames.purpose : "...",
+        AttributeNames.amount : "...",
+        AttributeNames.savings : "...",
+        AttributeNames.employment : "...",
+        AttributeNames.available_income : "...",
+        AttributeNames.other_debtors : "...",
+        AttributeNames.residence : "...",
+        AttributeNames.assets : "...",
+        AttributeNames.age : "...",
+        AttributeNames.other_loans : "...",
+        AttributeNames.housing : "...",
+        AttributeNames.previous_loans : "...",
+        AttributeNames.job : "...",
+        AttributeNames.people_liable : "...",
+        AttributeNames.NN_recommendation : "The AI's recommendation whether the loan application should be approved or rejected",
+        AttributeNames.NN_confidence : "Indicates how confident the AI is in it's decision",
     }
     return example_output
