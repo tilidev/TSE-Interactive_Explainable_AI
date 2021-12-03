a51e60c7ac61be22352c6dd49f6c0446bf07902f
Nicolas Kiefer <nicolaskiefer1@web.de>
2021-12-03 12:28:43 +0100

    commit to add frontend-prototype

---
+++ Frontend/frontend-prototype/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>Startseite</h1>
+    <p>Willkommen zur interaktiven erklärbaren AI.</p>
+    <p>Diese AI Applikation gibt Bewertungen zur Bewilligung von Bankkrediten auf der Basis von Wissen, was sie Trainingsdaten gelernt hat.</p>
+    <p>Außerdem werden zu den Entscheidungen, die die AI trifft Erklärungen von verschiedenen Gesichtspunkten aus gegeben und dargestellt, mit deren Hilfe sich die Enzscheidung nachvollziehen lässt .</p>
+    <router-link to="/level1"><button>Los Gehts</button></router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home', 
+  components: { }
+}
+</script>
+
+<style scoped>
+  button {
+     margin-top: 80px;
+     border: none;
+     background-color: blue;
+     color: white;
+     height: 60px;
+     width: 140px;
+     border-radius: 5%;
+  }
+</style>
---
+++ Frontend/frontend-prototype/src/views/Level1.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ratione dolorum sequi. Vitae id, molestiae, eos dolor dignissimos quo, velit sunt odio quos ut corporis sapiente officiis tenetur praesentium harum.</h1>
+  </div>
+</template>
