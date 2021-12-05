8846839ab54199f50aeec998455c616e3f6c31dd
felixhasse <felix.hasse@student.kit.edu>
2021-12-05 22:59:57 +0100

    Added a post request to the API's /table endpoint and display the response

--- Frontend/frontend-prototype/src/views/Level1.vue
+++ Frontend/frontend-prototype/src/views/Level1.vue
@@ -1,5 +1,39 @@
+<script>
+
+const axios = require("axios");
+
+export default {
+  data() {
+    return {
+      info: 'Text',
+    };
+  },
+  mounted() {
+    const reqBody = {
+      filter: [
+        {
+          attribute: "balance",
+          lower_bound: 0,
+          upper_bound: 0,
+        },
+        {
+          attribute: "balance",
+          values: ["string"],
+        },
+      ],
+      attributes: ["amount", "duration", "balance", "age", "employment"],
+      sort_by: "id",
+      limit: 20,
+      offset: 0,
+    };
+
+    axios.post("http://127.0.0.1:8000/table", reqBody).then((response) => (this.info = response)).catch(error => console.log(error));
+  },
+};
+</script>
+
 <template>
-  <div class="about">
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ratione dolorum sequi. Vitae id, molestiae, eos dolor dignissimos quo, velit sunt odio quos ut corporis sapiente officiis tenetur praesentium harum.</h1>
+  <div>
+    {{info}}
   </div>
 </template>
