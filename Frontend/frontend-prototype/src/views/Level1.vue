890798119f357b3234bc9611a017a8a50c5a4527
felixhasse <felix.hasse@student.kit.edu>
2021-12-06 17:39:49 +0100

    Created a simple table for Level 1

--- Frontend/frontend-prototype/src/views/Level1.vue
+++ Frontend/frontend-prototype/src/views/Level1.vue
@@ -1,39 +1,14 @@
 <script>
-
-const axios = require("axios");
+import DataTable from "../components/DataTable.vue";
 
 export default {
-  data() {
-    return {
-      info: 'Text',
-    };
-  },
-  mounted() {
-    const reqBody = {
-      filter: [
-        {
-          attribute: "balance",
-          lower_bound: 0,
-          upper_bound: 0,
-        },
-        {
-          attribute: "balance",
-          values: ["string"],
-        },
-      ],
-      attributes: ["amount", "duration", "balance", "age", "employment"],
-      sort_by: "id",
-      limit: 20,
-      offset: 0,
-    };
-
-    axios.post("http://127.0.0.1:8000/table", reqBody).then((response) => (this.info = response)).catch(error => console.log(error));
-  },
+  components: { DataTable },
 };
 </script>
 
 <template>
   <div>
-    {{info}}
+    <data-table></data-table>
   </div>
 </template>
+s
