133cc86000869da513568f5392ed7bf0586d1f0f
felixhasse <felix.hasse@student.kit.edu>
2021-12-11 16:12:07 +0100

    Removed the About view and replaced it with a dataset explorer page

--- Frontend/app/src/router/index.js
+++ Frontend/app/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import DatasetExplorer from '../views/DatasetExplorer.vue'
 
 const routes = [
   {
@@ -8,12 +9,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/dataset',
+    name: 'Dataset Explorer',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: DatasetExplorer
   }
 ]
 
---
+++ Frontend/app/src/views/DatasetExplorer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="dataset">
+    <h1>The table with the loan application data will be displayed here</h1>
+  </div>
+</template>
