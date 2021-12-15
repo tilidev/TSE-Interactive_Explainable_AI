5f88752362d0cdc751ec57b4bcc3ccc9996b7ab2
Isabelle Konrad <isabelle.konrad@student.kit.edu>
2021-12-15 09:19:02 +0100

    Added function for getting applications only with selected attributes

--- API/database_req.py
+++ API/database_req.py
@@ -1,8 +1,5 @@
-
 import sqlite3 as sql
 import json
-
-
 
 def create_connection(db):
     con = None
@@ -22,12 +19,28 @@ def get_applications(con, start, num = 10):
     return result
 
 def get_application(con, id, json_str = False):
-    con.row_factory = sql.Row
     c = con.cursor()
     query = 'SELECT * FROM applicants WHERE id = ' + str(id)
     rows = c.execute(query).fetchall()
-    con.commit()
-    con.close()
     if json_str:
-        rows = json.dumps( [dict(ix) for ix in rows] )
+        con.row_factory = sql.Row
+        rows = json.dumps([dict(ix) for ix in rows])
     return rows
+
+
+def get_applications_custom(con, start, attributes,  num = 10, json_str = False):   
+    c = con.cursor()
+    chosen = ''
+    if len(attributes) > 0:
+        for i in range(0,len(attributes)):
+            chosen += str(attributes[i])
+            chosen += ','
+        chosen = chosen[:-1]
+    end = int(start) + int(num)
+    query = 'SELECT ' + chosen + ' FROM applicants WHERE id >= ' + str(start) + ' AND id < ' + str(end) 
+    rows = c.execute(query).fetchall()
+    if json_str:
+        con.row_factory = sql.Row
+        rows = json.dumps([dict(ix) for ix in rows])
+    return rows
+
--- API/test.py
+++ API/test.py
@@ -1,12 +1,17 @@
 
 from database_req import create_connection
 from database_req import get_application
+from database_req import get_applications_custom
 
 
 
 
 con = create_connection('database.db')
-res = get_application(con, 0, json_str=True)
+#res = get_application(con, 0, json_str=True)
+l = ["balance", "duration", "history"]
+res = get_applications_custom(con, 0, l)
+
+
 print(res)
 
 
