06b0f2bfa50635efa66ee05753364edb11ead949
felixhasse <felix.hasse@student.kit.edu>
2021-12-16 11:46:44 +0100

    Added an outline button component

---
+++ Frontend/app/src/components/buttons/OutlineButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button
+      class="
+        btn
+        bg-white
+        hover:bg-gray-light
+        text-primary
+        px-4
+        py-2
+        rounded-md
+        text-lg
+        font-bold
+        m-4
+        border
+        border-primary
+      "
+    >
+      <slot />
+    </button>
+  </div>
+</template>
--- Frontend/app/src/views/DatasetExplorer.vue
+++ Frontend/app/src/views/DatasetExplorer.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="dataset">
     <h1>The table with the loan application data will be displayed here</h1>
-    <default-button>Example Button</default-button>
+    <default-button>Default Button</default-button>
     <clear-button>Clear Button</clear-button>
+    <outline-button>Outline Button</outline-button>
   </div>
 </template>
 
 <script>
 import DefaultButton from "../components/buttons/DefaultButton.vue";
 import ClearButton from "../components/buttons/ClearButton.vue";
-
+import OutlineButton from "../components/buttons/OutlineButton.vue"
 export default {
-  components: { DefaultButton, ClearButton },
+  components: { DefaultButton, ClearButton, OutlineButton },
 };
 </script>
