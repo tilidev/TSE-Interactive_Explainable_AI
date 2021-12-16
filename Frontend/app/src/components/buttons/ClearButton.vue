56dcb0898e51c737d2034fae74de0627e9e12c25
felixhasse <felix.hasse@student.kit.edu>
2021-12-16 11:43:28 +0100

    Added a Clear Button component

--- Frontend/app/src/components/buttons/ClearButton.vue
+++ Frontend/app/src/components/buttons/ClearButton.vue
@@ -3,9 +3,9 @@
     <button
       class="
         btn
-        bg-primary
-        hover:bg-primary-dark
-        text-white
+        bg-white
+        hover:bg-gray-light
+        text-primary
         px-4
         py-2
         rounded-md
@@ -18,8 +18,3 @@
     </button>
   </div>
 </template>
-
-<script>
-export default {
-};
-</script>
--- Frontend/app/src/views/DatasetExplorer.vue
+++ Frontend/app/src/views/DatasetExplorer.vue
@@ -2,13 +2,15 @@
   <div class="dataset">
     <h1>The table with the loan application data will be displayed here</h1>
     <default-button>Example Button</default-button>
+    <clear-button>Clear Button</clear-button>
   </div>
 </template>
 
 <script>
 import DefaultButton from "../components/buttons/DefaultButton.vue";
+import ClearButton from "../components/buttons/ClearButton.vue";
 
 export default {
-  components: { DefaultButton },
+  components: { DefaultButton, ClearButton },
 };
 </script>
