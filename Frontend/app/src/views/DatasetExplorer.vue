aff75ca22dff21dc5dc74d8a0feb93666a3aa0d5
felixhasse <felix.hasse@student.kit.edu>
2021-12-16 15:10:17 +0100

    Defined sample data that can be used for constructing the table

--- Frontend/app/src/views/DatasetExplorer.vue
+++ Frontend/app/src/views/DatasetExplorer.vue
@@ -1,19 +1,66 @@
 <template>
   <div class="dataset">
-    <data-table v-bind:data=tableData ></data-table>
+    <data-table v-bind:data="tableData"></data-table>
   </div>
 </template>
 
 <script>
-import DataTable from "../components/table/DataTable.vue"
-
+import DataTable from "../components/table/DataTable.vue";
 
 export default {
   data() {
     return {
-      tableData : [{id: "1", }],
+      tableData: {
+        labels: {
+          id: "#",
+          balance: "Balance",
+          duration: "Duration (months)",
+          amount: "Amount",
+          purpose: "Purpose",
+          NN_recommendation: "AI Recommendation",
+          NN_confidence: "AI Confidence",
+        },
+        descriptions: {
+          id: "Description",
+          balance: "Description",
+          duration: "Description",
+          amount: "Description",
+          purpose: "Description",
+          NN_recommendation: "Description",
+          NN_confidence: "Description",
+        },
+        rows: [
+          {
+            id: "1",
+            balance: "Above 200 EUR",
+            duration: "48",
+            amount: "14000 EUR",
+            purpose: "new car",
+            NN_recommendation: "Approve",
+            NN_confidence: 0.9,
+          },
+          {
+            id: "1",
+            balance: "Above 200 EUR",
+            duration: "48",
+            amount: "14000 EUR",
+            purpose: "new car",
+            NN_recommendation: "Approve",
+            NN_confidence: 0.9,
+          },
+          {
+            id: "1",
+            balance: "Above 200 EUR",
+            duration: "48",
+            amount: "14000 EUR",
+            purpose: "new car",
+            NN_recommendation: "Approve",
+            NN_confidence: 0.9,
+          },
+        ],
+      },
       requestBody: {},
-    }
+    };
   },
   components: { DataTable },
 };
