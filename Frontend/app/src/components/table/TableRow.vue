52859f47a4f983878fefa76189e0b6a3a75e9bc1
felixhasse <felix.hasse@student.kit.edu>
2021-12-18 23:36:42 +0100

    Changed the hover color for rows

--- Frontend/app/src/components/table/TableRow.vue
+++ Frontend/app/src/components/table/TableRow.vue
@@ -1,5 +1,5 @@
 <template>
-  <tr class="bg-white hover:bg-background">
+  <tr class="bg-white hover:bg-gray">
     <td class="py-5 px-10 text-primary">{{rowData.other.id}}</td>
     <td class="py-5 px-10" v-for="value in rowData.attributes" :key="value">{{ value }}</td>
     <td class="py-5 px-10 text-green-500 font-bold text-white" >{{rowData.other.NN_recommendation}}</td>
