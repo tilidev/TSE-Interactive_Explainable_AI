53271de879aeef93a7c82bdd94bd481e938345e6
felixhasse <felix.hasse@student.kit.edu>
2021-12-19 17:04:20 +0100

    Added FontAwesome solid icons to the project

--- Frontend/app/src/main.js
+++ Frontend/app/src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).use(router).mount('#app')
+library.add(fas);
+
+
+createApp(App).use(router).component("fa-icon", FontAwesomeIcon).mount('#app')
