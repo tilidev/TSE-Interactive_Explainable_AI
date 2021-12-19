039c870814885ed3135d337e27563ba577acd71c
felixhasse <felix.hasse@student.kit.edu>
2021-12-19 12:52:20 +0100

    Added shadow, padding and color to table elements

--- Frontend/app/src/components/table/TableHeader.vue
+++ Frontend/app/src/components/table/TableHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <tr class="rounded-md">
-    <th class="p-3" v-for="label in labels" :key="label">{{ label }}</th>
+  <tr class="rounded-md shadow">
+    <th class="py-5 px-10" v-for="label in labels" :key="label">{{ label }}</th>
   </tr>
 </template>
 
--- Frontend/app/src/components/table/TableRow.vue
+++ Frontend/app/src/components/table/TableRow.vue
@@ -1,9 +1,15 @@
 <template>
-  <tr class="bg-gray-light hover:bg-white border-2 border-primary">
-    <td class="p-3 text-primary">{{rowData.other.id}}</td>
-    <td class="p-3" v-for="value in rowData.attributes" :key="value">{{ value }}</td>
-    <td class="p-3" >{{rowData.other.NN_recommendation}}</td>
-    <td class="p-3">{{rowData.other.NN_confidence}}</td>
+  <tr class="bg-gray-light hover:bg-white border-2 border-primary shadow">
+    <td class="py-5 px-10 text-primary">{{ rowData.other.id }}</td>
+    <td class="py-5 px-10" v-for="value in rowData.attributes" :key="value">
+      {{ value }}
+    </td>
+    <td class="py-5 px-10 bg-positive-light font-bold">
+      {{ rowData.other.NN_recommendation }}
+    </td>
+    <td class="py-5 px-10 bg-cc-medium font-bold">
+      {{ rowData.other.NN_confidence }}
+    </td>
   </tr>
 </template>
 
@@ -12,8 +18,6 @@ export default {
   props: {
     rowData: Object,
   },
-  mounted() {
-
-  }
+  mounted() {},
 };
 </script>
