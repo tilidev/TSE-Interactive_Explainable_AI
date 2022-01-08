15a09b8d911c0015c48b2aaa469b78fe3a070ede
felixhasse <felix.hasse@student.kit.edu>
2022-01-08 11:47:53 +0100

    Added a computed property for the attribute categories

--- Frontend/app/src/components/InfoCard.vue
+++ Frontend/app/src/components/InfoCard.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="bg-white shadow-md p-4">instance: {{ instanceInfo }}</div>
+  <div class="bg-white shadow-md p-4">
+    <div>{{ attributeCategories }}</div>
+  </div>
 </template>
 
 <script>
 export default {
   props: { instanceInfo: Object, attributeData: Object },
+  computed: {
+    attributeCategories() {
+      const attrCat = {
+        financial: [],
+        personal: [],
+        loan: [],
+      };
+      for (const attr of Object.keys(this.attributeData.categories)) {
+        attrCat[this.attributeData.categories[attr]].push(attr);
+      }
+      return attrCat;
+    },
+  }
 };
 </script>
 
