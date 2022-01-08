89d26ca0dbb9c979ac01b833748310237c77464e
felixhasse <felix.hasse@student.kit.edu>
2022-01-08 12:36:07 +0100

    Optimize alignment and styling in the Info Card

--- Frontend/app/src/components/InfoCard.vue
+++ Frontend/app/src/components/InfoCard.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="bg-white shadow-md p-4">
-    <div class="flex text-left">
-      <div class="pr-8"
+  <div class="bg-white shadow-md p-8">
+    <div class="flex text-left text-text">
+      <div
+        class="pr-32 flex"
         v-for="(attributes, category) of attributeCategories"
         :key="category"
       >
-        <h3 class="capitalize font-bold">{{ category }}</h3>
-        <div v-for="attribute of attributes" :key="attribute"><span>{{attributeData.labels[attribute]}}: </span><span>{{instanceInfo[attribute]}}</span></div>
+        <span class="pr-8">
+          <h3 class="capitalize font-bold text-lg pb-4">{{ category }}</h3>
+          <div v-for="attribute of attributes" :key="attribute" class="text-primary">
+            {{ attributeData.labels[attribute] }}:
+          </div>
+        </span>
+        <span
+          ><h3 class="invisible pb-4 text-lg">Invisible Title</h3><div v-for="attribute of attributes" :key="attribute">
+            {{ instanceInfo[attribute] }}
+          </div></span
+        >
       </div>
     </div>
   </div>
