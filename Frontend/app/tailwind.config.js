507b54338adf0c0457b96c6cbc83c4a214ce872b
felixhasse <felix.hasse@student.kit.edu>
2022-01-08 13:02:06 +0100

    Changed the positive/negative semantic colors

--- Frontend/app/tailwind.config.js
+++ Frontend/app/tailwind.config.js
@@ -14,9 +14,9 @@ module.exports = {
       colors: {
         'primary': '#283252', // Primary accent color, currently blue
         'primary-dark' : '#050F2D', // Used for hovering over a button
-        'positive': '#2B7D2B', // Green, used to communicate approval
+        'positive': '#16C784', // Green, used to communicate approval
         'positive-light' : '#ABE2AB', // Lighter version of positive, should be used in table & charts
-        'negative' : '#BB0000', // Red, used to communicate rejection
+        'negative' : '#EA3943', // Red, used to communicate rejection
         'negative-light' : '#FF8888', // Lighter version of negative, should be used in table & charts
         'gray' : '#DFDFDF', // Used in table header
         'gray-light' : '#F2F2F2', // Used for background in table rows & chosen loan application section
