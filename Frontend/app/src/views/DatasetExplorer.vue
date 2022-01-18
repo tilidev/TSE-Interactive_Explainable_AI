95a6a70bb97d933eecc3229a4550ca7235fdb7a0
Nicolas Kiefer <nicolaskiefer1@web.de>
2022-01-18 10:45:36 +0100

    buttons do add to chosen customizations

--- Frontend/app/src/views/DatasetExplorer.vue
+++ Frontend/app/src/views/DatasetExplorer.vue
@@ -26,7 +26,7 @@
             <div class="grid grid-cols-3 gap-2 py-5">
               <div class="col-span-2">
                 <h4 class="text-lg font-bold flex flex-row pl-10">
-                Select up to 5 Attributes to display in the table
+                {{this.customizationMemory}}
               </h4>
               </div>
               <div class="pl-20">
@@ -43,37 +43,35 @@
             </div>
             <div class="grid grid-cols-8 gap-y-2 gap-x-2 pr-10">
               <div class="col-span-4 pt-4 pr-40">Loan</div>
-              <div><gray-button @click="addToCustomizations(amount)">Amount</gray-button></div>
-              <div><gray-button>Duration</gray-button></div>
-              <div><gray-button>Purpose</gray-button></div>
-              <div><gray-button>People Liable</gray-button></div>
+              <div><gray-button @click="addToCustomizations('amount')">Amount</gray-button></div>
+              <div><gray-button @click="addToCustomizations('duration')">Duration</gray-button></div>
+              <div><gray-button @click="addToCustomizations('purpose')">Purpose</gray-button></div>
+              <div><gray-button @click="addToCustomizations('people liable')">People Liable</gray-button></div>
               <div class="col-span-6"></div>
-              <div ><gray-button>Employment</gray-button></div>
-              <div><gray-button>Other debtors</gray-button></div>
+              <div ><gray-button @click="addToCustomizations('employment')">Employment</gray-button></div>
+              <div><gray-button @click="addToCustomizations('other debtors')">Other debtors</gray-button></div>
 
               <div class="col-span-4 pt-4 pr-40 pl-5">Personal</div>
-              <div><gray-button>Age</gray-button></div>
-              <div><gray-button>Residence</gray-button></div>
-              <div><gray-button>Job</gray-button></div>
-              <div><gray-button>Telephone</gray-button></div>
+              <div><gray-button @click="addToCustomizations('age')">Age</gray-button></div>
+              <div><gray-button @click="addToCustomizations('residence')">Residence</gray-button></div>
+              <div><gray-button @click="addToCustomizations('job')">Job</gray-button></div>
+              <div><gray-button @click="addToCustomizations('telephone')">Telephone</gray-button></div>
               <div class="col-span-7"></div>
-              <div><gray-button>Housing</gray-button></div>
+              <div><gray-button @click="addToCustomizations('housing')">Housing</gray-button></div>
 
               <div class="col-span-4 pt-4 pr-40 pl-5">Financial</div>
-              <div><gray-button>Savings</gray-button></div>
-              <div><gray-button>Assets</gray-button></div>
-              <div><gray-button>Balance</gray-button></div>
-              <div><gray-button>Available Income</gray-button></div>
+              <div><gray-button @click="addToCustomizations('savings')">Savings</gray-button></div>
+              <div><gray-button @click="addToCustomizations('assets')">Assets</gray-button></div>
+              <div><gray-button @click="addToCustomizations('balance')">Balance</gray-button></div>
+              <div><gray-button @click="addToCustomizations('available income')">Available Income</gray-button></div>
               <div class="col-span-5"></div>
-              <div><gray-button>History</gray-button></div>
-              <div><gray-button>Other loans</gray-button></div>
-              <div><gray-button>Previous loans</gray-button></div>
+              <div><gray-button @click="addToCustomizations('history')">History</gray-button></div>
+              <div><gray-button @click="addToCustomizations('other loans')">Other loans</gray-button></div>
+              <div><gray-button @click="addToCustomizations('previous loans')">Previous loans</gray-button></div>
             </div>
             <div class="flex flex-row-reverse">
               <div class="basis-1/2">
-                <outline-button @click="toggleCustomize = false"
-                  >Cancel</outline-button
-                >
+                <outline-button @click="cancelAttributes()">Cancel</outline-button>
               </div>
               <div class="basis-1/2">
                 <default-button>Apply Changes</default-button>
@@ -177,10 +175,14 @@ export default {
     }else{
       this.removeAttribute(attribute);
     }
-  },
+    },
     removeAttribute(attribute) {
-      this.customizationMemory.push(attribute);//to be changed so it removes
+      this.customizationMemory.splice(this.customizationMemory.indexOf(attribute),1)
+    },
+    cancelAttributes() {
+      this.customizationMemory = [];
     }
+
   },
   components: { OutlineButton, GrayButton, DefaultButton },
 }
