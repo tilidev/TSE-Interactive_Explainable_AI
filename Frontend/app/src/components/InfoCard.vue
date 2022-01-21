41d099054414847f37b7ce9968db54d860f27c0c
Felix Hasse <felix.hasse@student.kit.edu>
2022-01-21 14:16:15 +0100

    Added attributes and values to the instance info card

--- Frontend/app/src/components/InfoCard.vue
+++ Frontend/app/src/components/InfoCard.vue
@@ -1,9 +1,51 @@
 <template>
-  <div class="bg-white shadow-md p-8 grid gap-x-4 grid-cols-7 text-left">
-    <div class="font-bold col-span-2">Financial</div>
-    <div class="font-bold col-span-2">Personal</div>
-    <div class="font-bold col-span-2">Loan-specific</div>
-    <div class="font-bold col-span-1">AI Recommendation</div>
+  <div class="bg-white shadow-md p-8 grid grid-cols-auto grid-flow-col text-left">
+    <div class="font-bold col-span-2 col-start-1">Financial</div>
+    <div
+      class="col-start-1"
+      v-for="attribute in attributeCategories.financial"
+      :key="attribute"
+    >
+      {{ attributeData.labels[attribute] }}:
+    </div>
+    <div
+      class="col-start-2 capitalize"
+      v-for="attribute in attributeCategories.financial"
+      :key="attribute"
+    >
+      {{ instanceInfo[attribute] }}
+    </div>
+    <div class="font-bold col-span-2 col-start-3">Personal</div>
+    <div
+      class="col-start-3"
+      v-for="attribute in attributeCategories.personal"
+      :key="attribute"
+    >
+      {{ attributeData.labels[attribute] }}:
+    </div>
+    <div
+      class="col-start-4 capitalize"
+      v-for="attribute in attributeCategories.personal"
+      :key="attribute"
+    >
+      {{ instanceInfo[attribute] }}
+    </div>
+    <div class="font-bold col-span-2 col-start-5">Loan-specific</div>
+    <div
+      class="col-start-5"
+      v-for="attribute in attributeCategories.loan"
+      :key="attribute"
+    >
+      {{ attributeData.labels[attribute] }}:
+    </div>
+    <div
+      class="col-start-6 capitalize"
+      v-for="attribute in attributeCategories.loan"
+      :key="attribute"
+    >
+      {{ instanceInfo[attribute] }}
+    </div>
+    <div class="font-bold col-span- col-start-7">AI Recommendation</div>
   </div>
 </template>
 
@@ -16,8 +58,10 @@ export default {
         financial: [],
         personal: [],
         loan: [],
+        other: [],
       };
       for (const attr of Object.keys(this.attributeData.categories)) {
+        console.log(attrCat[this.attributeData.categories[attr]]);
         attrCat[this.attributeData.categories[attr]].push(attr);
       }
       return attrCat;
