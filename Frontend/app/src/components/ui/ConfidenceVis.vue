6c64ce397b615018b14428c92e8e489d31e5343d
Felix Hasse <felix.hasse@student.kit.edu>
2022-01-21 16:42:24 +0100

    Added missing blank spaces

--- Frontend/app/src/components/ui/ConfidenceVis.vue
+++ Frontend/app/src/components/ui/ConfidenceVis.vue
@@ -2,12 +2,12 @@
   <div v-if="confidence < 0.75" class="text-cc-low font-bold">
     <span class="rounded-lg bg-cc-low text-white px-2 py-1 mr-2"
       >{{ Math.round(confidence * 100) }}%</span
-    ><span>Low<span v-if="explicit">Confidence</span></span>
+    ><span>Low <span v-if="explicit">Confidence</span></span>
   </div>
   <div v-else-if="confidence >= 0.9" class="text-cc-high font-bold">
     <span class="rounded-lg bg-cc-high text-white px-2 py-1 mr-2"
       >{{ Math.round(confidence * 100) }}%</span
-    ><span>High<span v-if="explicit">Confidence</span></span>
+    ><span>High <span v-if="explicit">Confidence</span></span>
   </div>
   <div
     v-else-if="confidence >= 0.75"
