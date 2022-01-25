bf43f35e29f91e5fd49f52d87b824b7161eb8de7
Felix Hasse <felix.hasse@student.kit.edu>
2022-01-25 13:59:42 +0100

    Small style changes

--- Frontend/app/src/views/InstanceView.vue
+++ Frontend/app/src/views/InstanceView.vue
@@ -8,7 +8,7 @@
       @apply-modification="applyModification"
       @reset-instance="modifiedInstance = Object.assign({}, instanceInfo);"
     ></info-card>
-    <dice-explanation :instanceInfo="instanceInfo" class="my-4"></dice-explanation>
+    <dice-explanation :instanceInfo="instanceInfo" class="mb-4 mt-8"></dice-explanation>
   </div>
 </template>
 
