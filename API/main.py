5a628ff02c5da00041ddd5fdc5b932b329c64a2f
Tilio Schulze <tilio.schulze@gmail.com>
2022-02-23 12:10:08 +0100

    add comment

--- API/main.py
+++ API/main.py
@@ -165,6 +165,7 @@ async def schedule_explanation_generation(
 
     #TODO: assume that each attribute is in the instance_info, but only if shap and lime!!!
     #TODO: Check that instance id is provided and legal for dice requests
+    #TODO remove dice request
     job = Job(exp_type=exp_method, status=ResponseStatus.in_prog)
     job.task = {"instance" : instance, "num_features" : num_features, "num_cfs" : num_cfs, "is_modified" : is_modified}
     task_queue.put(job)
