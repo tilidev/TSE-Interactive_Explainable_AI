5b238c27c5de649ec41569ee03cbb511d265192f
Nicolas Kiefer <nicolaskiefer1@web.de>
2022-02-23 11:30:27 +0100

    Treemap tests

--- Frontend/app/src/views/TreeMapTestView4.vue
+++ Frontend/app/src/views/TreeMapTestView4.vue
@@ -1,5 +1,7 @@
 <template>
-  <svg class="svg"></svg>
+  <div>
+    <svg class="svg"></svg>
+  </div>
 </template>
 
 <script>
@@ -14,38 +16,70 @@ export default {
   },
   methods: {
     generateTreeMap() {
-      
-    document.addEventListener('DOMContentLoaded', () =>{
-    fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json")
-        .then(res=>res.json())
-        .then(res=>{
-            drawTreeMap(res);   
-        });
-    });
-    const drawTreeMap = (dataset)=>{  
-       const hierarchy = d3.hierarchy(dataset)
-                        .sum(d=>d.value)  //sum every child's values
-                        .sort((a,b)=>b.value-a.value) // and sort them in descending order 
-    
-    const treemap = d3.treemap()
-                  .size([800, 900]) // width: 400px, height:450px
-                  .padding(1);      // set padding to 1
-    
-    const root = treemap(hierarchy);
+      document.addEventListener("DOMContentLoaded", () => {
+        fetch(
+          "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json"
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            drawTreeMap(res);
+          });
+      });
 
-    const svg = d3.select("svg"); //make sure there's a svg element in your html file.
+      const drawTreeMap = (dataset) => {
+        const hierarchy = d3
+            .hierarchy(dataset)
+            .sum((d) => d.value) //sums every child values
+            .sort((a, b) => b.value - a.value), // and sort them in descending order
 
-              svg.selectAll("rect")
-                 .data(root.leaves())
-                 .enter()
-                 .append("rect")
-                 .attr("x", d=>d.x0)   
-                 .attr("y", d=>d.y0)
-                 .attr("width",  d=>d.x1 - d.x0)
-                 .attr("height", d=>d.y1 - d.y0)
-                 .attr("fill", "#5AB7A9")
-    }
-    }
-  }
-}
+
+          treemap = d3
+              .treemap()
+              .size([250, 225])
+              .padding(1),
+
+          root = treemap(hierarchy);
+
+        const categories = dataset.children.map((d) => d.name),
+          colors = [
+            "#1C1832",
+            "#9E999D",
+            "#F2259C",
+            "#347EB4",
+            "#08ACB6",
+            "#91BB91",
+            "#BCD32F",
+            "#75EDB8",
+            "#89EE4B",
+            "#AD4FE8",
+            "#D5AB61",
+            "#BC3B3A",
+            "#F6A1F9",
+            "#87ABBB",
+            "#412433",
+            "#56B870",
+            "#FDAB41",
+            "#64624F",
+          ],
+          colorScale = d3
+            .scaleOrdinal() // the scale function
+            .domain(categories) // the data
+            .range(colors); // the way the data should be shown
+
+        const svg = d3.select("svg"); //make sure there's a svg element in your html file
+
+        svg
+          .selectAll("rect")
+          .data(root.leaves())
+          .enter()
+          .append("rect")
+          .attr("x", (d) => d.x0)
+          .attr("y", (d) => d.y0)
+          .attr("width", (d) => d.x1 - d.x0)
+          .attr("height", (d) => d.y1 - d.y0)
+          .attr("fill", (d) => colorScale(d.data.category));
+      };
+    },
+  },
+};
 </script>
