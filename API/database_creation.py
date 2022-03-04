19db7cde412cfdf113b0574b6a2e26c9dad3dab6
Isabelle Konrad <isabelle.konrad@student.kit.edu>
2022-03-04 16:50:30 +0100

    Adding comment to explain database creation

--- API/database_creation.py
+++ API/database_creation.py
@@ -1,8 +1,16 @@
 from DataLoader_ey import createDataframeForDB
 import sqlite3 as sql
-from constants import rename_dict
+from constants import *
+from database_req import cf_response_format_db 
 
-con = sql.connect('database.db')
+"""This script can be run to create the database needed for the program.
+It creates the four tables applicants, dice, experiments and results.
+The applicants table is initialised with the loan applications from the german.csv data.
+The dice table is initialised with the pregenerated counterfactuals from cfs_response_format.json.
+The experiments table is used to store experiment information and the results table for the experiment results.
+"""
+
+con = sql.connect(db_path)
 c = con.cursor()
 #the create_query is only needed for initialisation not when just wanting to reset
 create_query_appl ='CREATE TABLE IF NOT EXISTS applicants (id INT, balance TEXT,duration INT,history TEXT,purpose TEXT,amount REAL,savings TEXT,employment TEXT,available_income TEXT,other_debtors TEXT,residence TEXT,assets TEXT,age INT,other_loans TEXT,housing TEXT,previous_loans TEXT,job TEXT,people_liable TEXT,telephone TEXT, NN_recommendation TEXT, NN_confidence REAL)'
@@ -21,4 +29,5 @@ c.execute(create_query_res)
 create_query_cf = 'CREATE TABLE IF NOT EXISTS dice (instance_id INT PRIMARY KEY, counterfactuals JSON);'
 c.execute(create_query_cf)
 con.commit()
+cf_response_format_db(con, 'Data/cfs_response_format.json')
 con.close()
