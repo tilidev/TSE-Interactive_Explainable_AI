96854a3fe4648e3ef8cbfd1d1243eb76b328d898
Felix Hasse <felix.hasse@student.kit.edu>
2022-03-09 12:33:37 +0100

    Removed unused import

--- Frontend/app/src/router/index.js
+++ Frontend/app/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
 import DatasetExplorer from "../views/DatasetExplorer.vue";
 import InstancePage from "../views/InstancePage.vue";
 import ExperimentPage from "../views/ExperimentPage.vue"
