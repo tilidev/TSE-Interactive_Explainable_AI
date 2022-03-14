cfb230cefc3564bd8222312ab03207703f7d2f56
Felix Hasse <felix.hasse@student.kit.edu>
2022-03-14 20:56:26 +0100

    Renamed category 'other' to 'prediction' to adjust to API change

--- Frontend/app/src/App.vue
+++ Frontend/app/src/App.vue
@@ -37,7 +37,7 @@ export default {
         financial: [],
         personal: [],
         loan: [],
-        other: [],
+        prediction: [],
       },
       reducedCategories: {
         financial: [],
