540e37d8cc4fe71a604205c8f701896f315abd75
Felix Hasse <felix.hasse@student.kit.edu>
2022-03-14 11:18:19 +0100

    Add authentication with backend

--- Frontend/app/src/components/admin/LoginScreen.vue
+++ Frontend/app/src/components/admin/LoginScreen.vue
@@ -1,31 +1,35 @@
 <template>
-<div>
-  <div class="font-bold text-lg mb-4">Please enter the admin password to enter</div>
-  <input class="mb-4 rounded border-2 p-2" v-model="userInput" type="password" placeholder="Password">
-  <default-button @click="clickLogin">Login</default-button>
+  <div>
+    <div class="font-bold text-lg mb-4">
+      Please enter the admin password to enter
+    </div>
+    <input
+      class="mb-4 rounded border-2 p-2"
+      v-model="userInput"
+      type="password"
+      placeholder="Password"
+    />
+    <default-button @click="clickLogin">Login</default-button>
   </div>
-
 </template>
 
 <script>
-import DefaultButton from '../buttons/DefaultButton.vue';
+import DefaultButton from "../buttons/DefaultButton.vue";
 export default {
   components: { DefaultButton },
+  inject: ["apiUrl"],
   methods: {
     clickLogin() {
-      if (this.userInput == this.password) {
-        this.$emit("logged-in");
-      }
-      else {
-        alert("Sorry, wrong password");
-        this.userInput = ""
-      }
-    }
+      const axios = require("axios");
+      axios
+        .get(this.apiUrl + "authenticate?pwd=" + this.userInput)
+        .then(() => this.$emit("logged-in"))
+        .catch(() => alert("Sorry, wrong password!"));
+    },
   },
   data() {
     return {
       userInput: "",
-      password: "password",
     };
   },
 };
