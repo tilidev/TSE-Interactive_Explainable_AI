79af9769e269151de2f69bd0883b96e5ebdf44fb
Nicolas Kiefer <nicolaskiefer1@web.de>
2022-03-14 13:57:12 +0100

    treemap detail switch(layout fix needed)

--- Frontend/app/src/views/InstanceView.vue
+++ Frontend/app/src/views/InstanceView.vue
@@ -9,13 +9,23 @@
       @apply-modification="applyModification"
       @reset-instance="modifiedInstance = Object.assign({}, instanceInfo)"
     ></info-card>
+    <div class="flex flex-row-reverse gap-y-4 pb-4 justify-start">
+      <outline-button @click="detailed = !detailed">Detail</outline-button>
+    </div>
     <dice-explanation
       v-if="instanceInfo.id != null && expType === 'dice'"
       :instanceInfo="instanceInfo"
       class="mb-4 mt-8"
     ></dice-explanation>
     <tree-map
-      v-else-if="Object.keys(instanceInfo).length"
+      v-else-if="Object.keys(instanceInfo).length && this.detailed == false"
+      :expType="expType"
+      :instance="instanceInfo"
+      :detailView="false"
+      :whatif="false"
+    ></tree-map>
+    <tree-map
+      v-else-if="Object.keys(instanceInfo).length && this.detailed == true"
       :expType="expType"
       :instance="instanceInfo"
       :detailView="true"
@@ -28,14 +38,16 @@
 import InfoCard from "../components/InfoCard.vue";
 import DiceExplanation from "../components/explanations/DiceExplanation.vue";
 import TreeMap from "../components/explanations/TreeMap.vue";
+import OutlineButton from '../components/buttons/OutlineButton.vue';
 
 export default {
   data() {
     return {
       modifiedInstance: {},
+      detailed: false
     };
   },
-  components: { InfoCard, DiceExplanation, TreeMap },
+  components: { InfoCard, DiceExplanation, TreeMap, OutlineButton },
   methods: {
     applyModification(modification) {
       this.modifiedInstance[modification["attribute"]] = modification["value"];
