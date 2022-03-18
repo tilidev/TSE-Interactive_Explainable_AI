31999bb6176d7db37dd5df879a1a07b2db9a014f
Tilio Schulze <tilio.schulze@gmail.com>
2022-03-18 22:16:43 +0100

    further testing

--- API/test_api.py
+++ API/test_api.py
@@ -1,7 +1,12 @@
 # To run these tests, cd to the API folder, run the main app
 # and once the API has finished startup, run pytest in another terminal session
+
+#imports
+
 import requests
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY, HTTP_404_NOT_FOUND, HTTP_202_ACCEPTED, HTTP_400_BAD_REQUEST
+import time
+import numpy as np
 
 # Helper methods
 
@@ -49,3 +54,36 @@ def test_table_bad_request():
 
     res = r_post("table", request_data)
     assert res.status_code == HTTP_400_BAD_REQUEST
+
+def test_table_normal():
+    pass
+
+def test_time_table():
+    request_data = """{"filter":[
+        {"attribute":"assets",
+        "values":["car","real estate"]},
+        {"attribute":"housing",
+        "values":["for free","own"]},
+        {"attribute":"employment",
+        "values":["unemployed","between 1 and 4 years","more than 7 years"]},
+        {"attribute":"amount",
+        "lower_bound":1140,
+        "upper_bound":8612}],
+        "attributes":["balance","duration","amount","age","savings"],
+        "sort_by":"id",
+        "sort_ascending":true,
+        "desc":false,
+        "limit":100,
+        "offset":0}"""
+    times = []
+    for _ in range(100):
+        start = time.time_ns()
+        res = r_post("table", request_data)
+        end = time.time_ns()
+        assert (end - start)/1e6 < 10 # assert less than 10 milliseconds
+        times.append((end - start)/1e6)
+    print(np.mean(times))
+    assert len(res.json()) > 0
+
+
+
